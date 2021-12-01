25110aa342c369afea9d1851528c505c00f18d17
Marc Thomas <marc.thomas@shopify.com>
2021-12-01 12:43:44 +0000

    chore: remove

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env zsh
 
-install_homebrew() {
-    if ! command -v brew &> /dev/null; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew update
-        brew upgrade
-    fi
-}
+# install_homebrew() {
+#     if ! command -v brew &> /dev/null; then
+#         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#         brew update
+#         brew upgrade
+#     fi
+# }
 
-install_diffmerge() {
-    if ! command -v diffmerge &> /dev/null; then
-        echo "\n –– installing diffmerge –– \n"
-        brew cask install diffmerge
-    fi
-}
+# install_diffmerge() {
+#     if ! command -v diffmerge &> /dev/null; then
+#         echo "\n –– installing diffmerge –– \n"
+#         brew cask install diffmerge
+#     fi
+# }
 
 install_deps() {
-    install_homebrew
-    install_diffmerge
+    echo "Dotfiles installed"
+    # install_homebrew
+    # install_diffmerge
 }
 
 if [ $SPIN ]; then
